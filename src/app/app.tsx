f14b651b8bc717f409668c18ebb8e4b2abfcf111
Orion Girault <orion.girault2@soprasteria.com>
2025-05-15 09:12:08 +0200

    feat: test B 2

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,6 +7,7 @@ import { JSX } from "react";
 import { Route, Routes } from "react-router";
 
 export const App: () => JSX.Element = () => {
+    console.log("et a la racine");
     return (
         <div className="h-full w-full">
             <Layout>
